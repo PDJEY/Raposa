30c9f97657dd9b99c133f583368988f0fd0ee71a
DJGPT <69373255+DJGPT@users.noreply.github.com>
2023-05-04 22:41:19 +0100

    Basic movement done
    Done basic movement. Jumping is missing. Might be considered optional

--- Fox/Assets/Scrips/PlayerController.cs
+++ Fox/Assets/Scrips/PlayerController.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController controller;
+    GameObject ground;
     public float speed = 10f;
-
+    public float jumpF = 10f;
+    public float gravity = 100f;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        ground = GetComponent<GameObject>();
     }
 
+    void Update()
+    {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+        Debug.Log(hMove);
+        rb.velocity = new Vector3(-(hMove * speed), rb.velocity.y, -(vMove * speed));
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed*Time.deltaTime, Input.GetAxis("Vertical") * speed*Time.deltaTime, 0);
 
-        controller.Move(movement);
     }
 }
