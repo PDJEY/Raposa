43103a6eb1ef20e38f125cf8f5c9d6741ba5fc73
DJGPT <69373255+DJGPT@users.noreply.github.com>
2023-05-07 04:02:32 +0100

    Movement finished
    O utilizador pode mexer-se e saltar

--- Fox/Assets/Scrips/PlayerController.cs
+++ Fox/Assets/Scrips/PlayerController.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    GameObject ground;
-    public float speed = 10f;
-    public float jumpF = 10f;
-    public float gravity = 100f;
-    public Rigidbody rb;
+    public CharacterController controller; //vai buscar o controlador do jogador
+    public Transform groundCheck; //vai buscar o objeto vazio
+
+    public float speed = 12f; //velocidade de movimento
+    public float gravity = 10f; //constante de queda
+    public float jumpHeight = 3f; //alcance maximo de salto
+    public float detectRadius = 0.2f; //raio que forma a circunferencia no objeto vazio que detecta o chão
+    public LayerMask detection; //define o que pode detetar
+    bool isGrounded; //guarda se está guardado
+
+    Vector3 velo; //guarda a velocidade do objeto
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        ground = GetComponent<GameObject>();
+
     }
 
     void Update()
     {
-        float hMove = Input.GetAxis("Horizontal");
-        float vMove = Input.GetAxis("Vertical");
-        Debug.Log(hMove);
-        rb.velocity = new Vector3(-(hMove * speed), rb.velocity.y, -(vMove * speed));
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, detectRadius, detection); //deteta se esta a colidir com algo
 
+        if (isGrounded && velo.y < 0)
+        {
+            velo.y = -2f;
+        }
+
+        float hMove = Input.GetAxis("Horizontal"); //deteta se as teclas A ou D estão a ser premidas
+        float vMove = Input.GetAxis("Vertical"); //deteta se as teclas S ou W estão a ser premidas
+
+        velo.y += -gravity * Time.deltaTime; //calcula a velocidade de queda
+        Vector3 movement = transform.right * -vMove + transform.forward * hMove; //calcula o movimento em X e em Z
+
+        controller.Move(movement * speed * Time.deltaTime); 
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            velo.y = Mathf.Sqrt(jumpHeight * -2f * -gravity); //define a velocidade necessaria pra alcançar jumpHeight
+        }
+
+        controller.Move(velo * Time.deltaTime);
+        
     }
 }
